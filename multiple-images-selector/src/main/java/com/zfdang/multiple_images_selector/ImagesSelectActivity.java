83de65a01882b2a411d6151d0d77e17ab6822725
Zhengfa DANG <dantifer@gmail.com>
2016-04-12 14:32:27 +0800

    Initial commit

---
+++ multiple-images-selector/src/main/java/com/zfdang/multiple_images_selector/ImagesSelectActivity.java
@@ -0,0 +1,13 @@
+package com.zfdang.multiple_images_selector;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImagesSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_images_select);
+    }
+}
